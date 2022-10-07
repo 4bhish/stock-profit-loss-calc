4b39ab12f5c9c36c58f9a8e0ff5fcc13d13f06ab
4bhish <108976136+4bhish@users.noreply.github.com>
2022-10-07 20:38:58 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+const ipInput = document.querySelector("#initial-price");
+const sqInput = document.querySelector("#stock-quantity");
+const cpInput = document.querySelector("#current-price");
+const btnTellme = document.querySelector("#tellme");
+const  outputText = document.querySelector("#output");
